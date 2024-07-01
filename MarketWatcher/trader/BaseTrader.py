8cfa74bc272f1cb233186570be17c896a20e023a
tani3010 <tani3010@gmail.com>
2024-07-02 07:43:37 +0900

    release html

--- MarketWatcher/trader/BaseTrader.py
+++ MarketWatcher/trader/BaseTrader.py
@@ -81,12 +81,12 @@ class BaseTrader(object):
         finally:
             return ret
 
-    def fetch_fee(self, result, symbol, price, amount, takerOrMaker='taker', params={}):
+    def fetch_fee(self, result, symbol, price, amount, taker_or_maker='taker', params={}):
         ret = False
         try:
             type_ = None  # this is not used in public API
             side = None  # this is not used in public API
-            tmp_result = self.exchange.calculate_fee(symbol, type_, side, amount, price, takerOrMaker, params)
+            tmp_result = self.exchange.calculate_fee(symbol, type_, side, amount, price, taker_or_maker, params)
             self.copy_dict(tmp_result, result)
             ret = True
             logger.info('completed.')
@@ -108,7 +108,7 @@ class BaseTrader(object):
     def send_sell_order_limit(self, result, symbol, price, amount, is_post_only=True, params={}):
         return self.send_order(result, symbol, price, amount, False, False, is_post_only, params)
 
-    def calculate_tradable_amount(self, result, symbol, price, side, type, takerOrMaker='taker', safe_margin=0.05):
+    def calculate_tradable_amount(self, result, symbol, price, side, type_, taker_or_maker='taker', safe_margin=0.005):
         ret = False
         result_balance = None
         result_fee = None
