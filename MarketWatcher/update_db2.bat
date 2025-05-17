cd434cd09fe191b986e104cff7126ee8e1a6da8e
tani3010 <tani3010@gmail.com>
2025-05-17 10:33:51 +0900

    release html

--- MarketWatcher/update_db2.bat
+++ MarketWatcher/update_db2.bat
@@ -22,7 +22,7 @@ git push origin main
 rem ##### remove output #####
 del /S /Q .\MarketWatcher\output\backtest
 del /S /Q .\MarketWatcher\output\forwardtest
-timeout /t 3000 > nul
+timeout /t 4000 > nul
 
 rem ##### resume dropbox #####
 start "Restart Dropbox" %DROPBOX_PATH%\Dropbox.exe -B
