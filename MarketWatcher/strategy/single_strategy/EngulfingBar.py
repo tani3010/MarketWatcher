a2f99f3c1f2ce5f190480271bede8dde4fddc169
tani3010 <tani3010@gmail.com>
2026-01-05 00:08:04 +0900

    release html

---
+++ MarketWatcher/strategy/single_strategy/EngulfingBar.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from .BaseStrategy import BaseStrategy
+from logging import getLogger
+logger = getLogger(__name__)
+
+class EngulfingBar(BaseStrategy):
+    def __init__(self, broker, data, params):
+        super().__init__(broker, data, params)
+        self.strategy_name = 'EngulfingBar'
+
+    def next(self):
+        super().next()
+
+        if self.data.Open[-1] < self.data.Close[-2] < self.data.Open[-2] < self.data.Close[-1]:
+            self.buy(tp=self.data.High[-1])
+
+        elif self.data.Close[-1] < self.data.Open[-2] < self.data.Close[-2] < self.data.Open[-1]:
+            self.sell(tp=self.data.Low[-1] * (1 - 0.0011))
